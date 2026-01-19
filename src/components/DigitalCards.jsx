defbeac48164687e7e4a24131734f398f265aa56
tanxueyi99 <tanxueyi99@gmail.com>
2026-01-19 17:29:48 +0800

    Overall optimization

--- src/components/DigitalCards.jsx
+++ src/components/DigitalCards.jsx
@@ -18,11 +18,17 @@ function DigitalCards({
     <div
       className="relative h-full w-full cursor-pointer group overflow-hidden transition-transform duration-300 hover:scale-106"
       onClick={handleClick}
+      style={{
+        backfaceVisibility: "hidden",
+        transform: "translate3d(0, 0, 0)",
+        willChange: "transform",
+      }}
     >
       <img
         src={imageSrc}
         alt={imageAlt}
         className="w-full h-full object-cover"
+        style={{ backfaceVisibility: "hidden" }}
       />
       <div className="absolute inset-0 pointer-events-none neon-glow-orange-bg z-20"></div>
       <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black via-black/70 to-transparent text-white p-6 pt-12 transition-all duration-300 group-hover:pb-8 pb-4">
